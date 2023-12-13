1b3d814c1ec8bf21ce61729d080de9bc21c07fa1
SetOfAllSets <NotTelling>
2023-12-13 11:09:14 -0800

    fixed list_posts on main domain, cloudflare redirects port 3000 to 80 for some reason so it shat itself using the api

--- html/files/internal/js/blog/list_posts.js
+++ html/files/internal/js/blog/list_posts.js
@@ -1,11 +1,5 @@
 async function getList() {
-    let response;
-    try {
-        response = await fetch("https://" + hostname + ":3000/api/blog_post_list");
-    }
-    catch(err) {
-        response = await fetch("https://api." + hostname + ":3000/api/blog_post_list");
-    }
+    let response = await fetch("https://api." + hostname + ":3000/api/blog_post_list");
     response = await response;
     let list = await response.json();
     return list;
